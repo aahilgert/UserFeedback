82d179e77cbc074c1636cb5ff05f103244ea7bcd
Arthur Ayestas Hilgert <aahilgert@gmail.com>
2020-02-19 16:43:48 -0500

    migrated and fixed path import

--- user_feedback/urls_test.py
+++ user_feedback/urls_test.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url, path
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 from . import urls
 
 from . import views_test
